e2a92247a9d1f8534f217b6d529fa71570f004d4
Boris Mann <boris@bmannconsulting.com>
2021-02-20 18:39:44 -0800

    Initial commit of setting up indiekit

---
+++ Procfile
@@ -0,0 +1,1 @@
+node indiekit.config.js
---
+++ indiekit.config.js
@@ -0,0 +1,10 @@
+import {Indiekit} from '@indiekit/indiekit';
+
+// Create a new indiekit instance
+const indiekit = new Indiekit();
+
+// Create a server
+const server = indiekit.server();
+
+// Export server
+export default server;
